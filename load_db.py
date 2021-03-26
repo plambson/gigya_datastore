02be7f20560022f5e6df5bf91c7249d78b89b146
paul <paullambson@gmail.com>
2021-03-26 09:53:55 -0400

    adding README.md

--- load_db.py
+++ load_db.py
@@ -5,15 +5,14 @@ from sqlalchemy.orm import sessionmaker
 import random
 import names
 
-
-
-
 engine = create_engine('sqlite:///payload.db', echo=False)
 
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
 # create a Session
 session = Session()
+
+
 def make_payload():
     if not Payload.__table__.exists(engine):
         Payload.__table__.create(engine)
@@ -21,15 +20,16 @@ def make_payload():
     else:
         print('{} exists'.format(Payload.__tablename__))
 
-def  load_payload():
+
+def load_payload():
     full = []
-    for i  in range(100):
+    for i in range(100):
         out = {}
-        out['gigya_id'] = random.randrange(100000,120000)
+        out['gigya_id'] = random.randrange(100000, 120000)
         out['docs'] = []
-        for ii in range(random.randrange(2,7)):
+        for ii in range(random.randrange(2, 7)):
             docs = {}
-            docs['MRN'] =random.randrange(2000,7000)
+            docs['MRN'] = random.randrange(2000, 7000)
             docs['name'] = names.get_full_name()
             out['docs'].append(docs)
         full.append(out)
@@ -41,10 +41,3 @@ def  load_payload():
 if __name__ == "__main__":
     make_payload()
     load_payload()
-
-
-[ p.payload for p in session.query(Payload).filter(Payload.gigya_id==117792)]
-
-
-session.query(Payload.payload).filter(Payload.gigya_id==117792).first()[0]
-
